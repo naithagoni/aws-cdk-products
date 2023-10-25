de697ce94a4c72740acd3416b3035f78a7d63c30
naresh.aithagoni <naresh.aithagoni@accenture.com>
2023-10-25 16:48:42 +0200

    Scaffolding the project

---
+++ test/aws-cdk-products.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkProducts from '../lib/aws-cdk-products-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-products-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkProducts.AwsCdkProductsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
