9d6c56b300023715d8ca9e3ca5966ddc38fda01d
Naresh Aithagoni <naithagoni91@gmail.com>
2023-12-28 15:24:59 +0100

    refactor(): POST and PUT methods

--- backend/app.js
+++ backend/app.js
@@ -8,14 +8,18 @@ import {
   UpdateCommand,
   DeleteCommand,
 } from "@aws-sdk/lib-dynamodb";
-import { marshall, unmarshall } from "@aws-sdk/util-dynamodb";
+import { marshall } from "@aws-sdk/util-dynamodb";
 
 const app = express();
 app.use(express.json());
 
 // Create a DynamoDB client
 const dbClient = new DynamoDBClient({ region: process.env.REGION });
-const docClient = DynamoDBDocumentClient.from(dbClient);
+const docClient = DynamoDBDocumentClient.from(dbClient, {
+  marshallOptions: {
+    removeUndefinedValues: true,
+  },
+});
 
 const TABLE_NAME = process.env.DYNAMO_TABLE_NAME;
 
@@ -58,11 +62,36 @@ app.get("/items/:id", async (req, res) => {
   }
 });
 
+/**
+ * @typedef {Object} UserDetails
+ * @property {string} itemId
+ * @property {string} userName
+ * @property {string} email
+ * @property {string} password
+ * @property {FullName} fullName
+ * @property {string} phone
+ * @property {Address} address
+ */
+
+/**
+ * @typedef {Object} FullName
+ * @property {string} firstName
+ * @property {string} lastName
+ */
+
+/**
+ * @typedef {Object} Address
+ * @property {string} street
+ * @property {string} zipcode
+ * @property {string} city
+ */
 app.post("/items", async (req, res) => {
   try {
+    /** @type {UserDetails} */
+    const userData = req.body;
     const params = {
       TableName: TABLE_NAME,
-      Item: req.body,
+      Item: userData,
     };
     await docClient.send(new PutCommand(params));
     res.status(201).json({ message: "Item created successfully" });
@@ -74,30 +103,72 @@ app.post("/items", async (req, res) => {
 
 app.put("/items/:id", async (req, res) => {
   try {
-    const updatedAttributes = {
-      itemName: req.body.itemName,
-      itemSize: req.body.itemSize,
+    let updatedAttributes = {
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password,
+      fullName: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      },
+      phone: req.body.phone,
+      address: {
+        street: req.body.street,
+        zipcode: req.body.zipcode,
+        city: req.body.city,
+      },
     };
+
+    // Construct the update expression dynamically
+    let updateExpression = "set";
+    let expressionAttributeValues = {};
+
+    const constructUpdateExpression = (obj, prefix = "") => {
+      Object.keys(obj).forEach((key) => {
+        const value = obj[key];
+        const attributeName = prefix ? `${prefix}.${key}` : key;
+
+        if (typeof value === "object" && !Array.isArray(value)) {
+          // Handle nested attributes
+          constructUpdateExpression(value, attributeName);
+        } else {
+          // Handle top-level attributes
+          updateExpression += ` ${attributeName} = :${attributeName},`;
+          expressionAttributeValues[`:${attributeName}`] = value;
+        }
+      });
+    };
+
+    constructUpdateExpression(updatedAttributes);
+
+    // Remove the trailing comma from the update expression
+    updateExpression = updateExpression.slice(0, -1);
 
     const params = {
       TableName: TABLE_NAME,
       Key: {
         itemId: req.params.id,
       },
-      UpdateExpression:
-        "SET " +
-        Object.keys(updatedAttributes)
-          .map((key) => `#${key} = :${key}`)
-          .join(", "),
-      ExpressionAttributeNames: Object.fromEntries(
-        Object.keys(updatedAttributes).map((key) => [`#${key}`, key])
-      ),
-      ExpressionAttributeValues: marshall(updatedAttributes),
+      UpdateExpression: updateExpression,
+      ExpressionAttributeValues: marshall(expressionAttributeValues),
+      ReturnValues: "ALL_NEW",
     };
 
     const command = new UpdateCommand(params);
     await docClient.send(command);
-    res.status(201).json({ message: "Item updated successfully" });
+
+    // Include the updated item in the response
+    if (res && res.Attributes) {
+      res.status(201).json({
+        message: "Item updated successfully",
+        updatedItem: res.Attributes,
+      });
+    } else {
+      res.status(201).json({
+        message: "Item updated successfully",
+        updatedItem: res,
+      });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "An error occurred" });
